af0385c02ce86724e14878ae0da403807a595781
LukaszSawala <lukaszsawala2003@gmail.com>
2024-12-06 12:34:13 +0100

    changed the implementation for the presentation

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from pathlib import Path
 import numpy as np
 import torch
 from citylearn.citylearn import CityLearnEnv
-from citylearn.agents.sac import SAC
+from stable_baselines3.sac import SAC as Agent  # remove later
+from citylearn.wrappers import NormalizedObservationWrapper, StableBaselines3Wrapper # same
+
 
 from agents.random_agent import RandomAgent
 
@@ -18,11 +20,45 @@ if __name__ == "__main__":
     env = CityLearnEnv(
         schema=schema_path, root_directory=root_directory, random_seed=SEED
     )
-    #model = RandomAgent(env)
-    model = SAC(env)
 
+    # EXTRA CODE FOR FRIDAY: REMOVE LATER -------------------------------------------
+    env = NormalizedObservationWrapper(env)
+    env = StableBaselines3Wrapper(env)
+    model = Agent('MlpPolicy', env)
+
+    # train
+    episodes = 2
+    model.learn(total_timesteps=env.unwrapped.time_steps*episodes)
+
+    # test
+    observations, _ = env.reset()
+
+    rewards = []
+    while not env.unwrapped.terminated:
+        actions, _ = model.predict(observations, deterministic=True)
+        observations, reward, _, _, _ = env.step(actions)
+        rewards.append(reward)
+
+    # plot rewards
+    import matplotlib.pyplot as plt
+    plt.plot(rewards)
+    plt.show()
+
+    # evaluate
+    kpis = env.unwrapped.evaluate()
+    kpis = kpis.pivot(index='cost_function', columns='name', values='value').round(3)
+    kpis = kpis.dropna(how='all')
+    print(kpis)
+
+    exit(0)
+    # END OF EXTRA CODE --------------------------------------------------------------
+
+    model = RandomAgent(env)
     model.learn(episodes=20)
 
     random_agent_data = model.evaluate_agent()
 
     print(random_agent_data)
+
+
+
