fixed filling the replay buffer, fixed custom reward calculation

src/agents/sac.py
@@ -83,10 +83,8 @@ class SACAgent(Agent):
         if self.total_steps < self.exploration_timesteps:
             # return action_space_dim number of values between -1 and 1
             print("selecting random action")
-            return np.array([0,0,0,0,0,0,0,0,0])
             actions_nr = self.action_space_dim
-            actions = (torch.rand((1, actions_nr)) * 0.1 - 0.05).cpu().numpy()[0]
-            #actions = (torch.rand((1, actions_nr)) * 2 - 1).cpu().numpy()[0]
+            actions = (torch.rand((1, actions_nr)) * 0.00001 - 0.000005).cpu().numpy()[0]
             print(actions)
             return actions
 

src/reward_function.py
@@ -200,7 +200,7 @@ class ComfortReward(RewardFunction):
 
 
 class CustomRewardFunction(ComfortReward):
-    r"""Custom reward function class.
+    """Custom reward function class.
     Combines :py:class:`citylearn.reward_function.ComfortReward (comfort of the occupants)` 
     and :py:class:`citylearn.reward_function.RewardFunction (electricity consumption)`
 
@@ -212,9 +212,18 @@ class CustomRewardFunction(ComfortReward):
         Other keyword arguments for custom reward calculation.
     """
     
-    def __init__(self, env_metadata: Mapping[str, Any], **kwargs):
+    def __init__(self, env_metadata: Mapping[str, Any], **kwargs) -> None:
+        """ Initialize the custom reward function. """
         super().__init__(env_metadata, **kwargs)
     
     def calculate(self, observations: List[Mapping[str, Union[int, float]]]) -> List[float]:
-        # fix lol
-        return super().calculate(observations) + RewardFunction.calculate(self, observations)
+        """ 
+        Calculates the custom reward using a simple sum.
+        TODO: APPLY WEIGHTING (comfort reward can be even ~50x electricity reward in magnitude),
+        which is why I reduced it by 20 for now.
+        """
+        rewards_comfort = ComfortReward.calculate(self, observations) 
+        rewards_electricity = RewardFunction.calculate(self, observations)
+        rewards_comfort = list(map(lambda x: x/20, rewards_comfort))
+        reward_sum = list(map(np.add, rewards_comfort, rewards_electricity))
+        return reward_sum