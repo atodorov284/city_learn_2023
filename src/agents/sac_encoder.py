5048962046a5d33876ff0e2c12ed09231c29b09a
03chrisk <chris.kobriger@gmail.com>
2024-12-14 15:14:25 +0100

    removing redundant files and adding day reward plotting

--- src/agents/sac_encoder.py
+++ src/agents/sac_encoder.py
@@ -8,4 +8,43 @@ from agents.base_agent import Agent
 from utils.replay_buffer import ReplayBuffer
 
 class SACEncoder(nn.Module):
-    pass
+    def __init__(
+        self, observation_space_dim: int, output_dim: int, hidden_dim: int
+    ) -> None:
+        """Initialize the encoder network"""
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        super(SACEncoder, self).__init__()
+
+        self.fc1 = nn.Linear(observation_space_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
+
+        self._initialize_weights()
+        
+        self.to(self.device)
+
+    def _initialize_weights(self):
+        """
+        Initialize the weights of the network layers using Xavier initialization.
+        """
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.xavier_uniform_(self.fc3.weight)
+
+
+    def forward(self, observation: torch.Tensor) -> torch.Tensor:
+        """
+        Compute a compressed representation of a state
+
+        Args:
+            state (torch.Tensor): Input state
+
+        Returns:
+            Compressed state representation
+        """
+        q1 = F.relu(self.fc1(observation))
+        q1 = F.relu(self.fc2(q1))
+        q1 = self.fc3(q1)
+
+        return q1
--- src/test_sac_agents.py
+++ src/test_sac_agents.py
@@ -219,7 +219,8 @@ def plot_rewards(rewards: list[float], agent_type: str = "centralized", plot_fol
     
     plt.axhline(y=0, color='k', linestyle='--', alpha=0.3)
     
-    plt.ticklabel_format(axis='x', scilimits=(0,0))
+    ax = plt.gca()
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, p: format(int(x), ',')))
     
     plt.tight_layout()
     #save in directory plots
@@ -230,17 +231,17 @@ def plot_rewards(rewards: list[float], agent_type: str = "centralized", plot_fol
 if __name__ == "__main__":
     
     # Create the environments
-    centralized_env = create_environment(central_agent=True, SEED=0,  path="data/citylearn_challenge_2023_phase_1")
-    #decentralized_env = create_environment(central_agent=False, SEED=0,  path="data/citylearn_challenge_2023_phase_1")
+    #centralized_env = create_environment(central_agent=True, SEED=0,  path="data/citylearn_challenge_2023_phase_1")
+    decentralized_env = create_environment(central_agent=False, SEED=0,  path="data/citylearn_challenge_2023_phase_1")
     
     # Create the agents
-    centralized_agent = create_agents(centralized_env, central_agent=True)
-    #decentralized_agent = create_agents(decentralized_env, central_agent=False)
+    #centralized_agent = create_agents(centralized_env, central_agent=True)
+    decentralized_agent = create_agents(decentralized_env, central_agent=False)
     
     # Train the agent
-    rewards_centralized, episode_rewards_centralized, daily_rewards_centralized = train_sac_agent(centralized_env, centralized_agent, episodes=6, central_agent=True)
-    #rewards_decentralized, episode_rewards_decentralized = train_sac_agent(decentralized_env, decentralized_agent, episodes=6, central_agent=False)
+    #rewards_centralized, episode_rewards_centralized, daily_rewards_centralized = train_sac_agent(centralized_env, centralized_agent, episodes=15, central_agent=True)
+    rewards_decentralized, episode_rewards_decentralized, daily_rewards_decentralized = train_sac_agent(decentralized_env, decentralized_agent, episodes=15, central_agent=False)
     
     # Plot the rewards
-    plot_rewards(daily_rewards_centralized, agent_type="centralized", plot_folder="plots/")
-    #plot_rewards(rewards_decentralized, agent_type="decentralized", plot_folder="plots/")
+    #plot_rewards(daily_rewards_centralized, agent_type="centralized", plot_folder="plots/")
+    plot_rewards(daily_rewards_decentralized, agent_type="decentralized", plot_folder="plots/")
